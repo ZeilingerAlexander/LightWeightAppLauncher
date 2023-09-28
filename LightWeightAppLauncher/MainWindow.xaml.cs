9ab9b4f7fcdefd6d6b00b243a9ada8ab309fb1e7
ZeilingerAlexander <116539631+ZeilingerAlexander@users.noreply.github.com>
2023-09-28 07:46:38 +0200

    Finished Launching Apps, Restructured Mainwindow design

--- LightWeightAppLauncher/MainWindow.xaml.cs
+++ LightWeightAppLauncher/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 namespace LightWeightAppLauncher
 {
@@ -9,7 +12,7 @@ namespace LightWeightAppLauncher
     /// </summary>
     public partial class MainWindow : Window
     {
-        List<string> AllSourcePaths = new List<string>();
+        Dictionary<int, string> AllSourcePaths = new Dictionary<int, string>();
         static readonly string _DirectoryConfigPath = Directory.GetCurrentDirectory().ToString() + "\\HV_AppLauncher";
         static readonly string _configPath = _DirectoryConfigPath + "\\config.hv";
         public MainWindow()
@@ -21,31 +24,67 @@ namespace LightWeightAppLauncher
         }
         void LoadConfig()
         {
+            AllSourcePaths.Clear();
+            int CurrentIndex = 0;
             foreach (string str in File.ReadLines(_configPath))
             {
-                AllSourcePaths.Add(str);
+                AllSourcePaths.Add(CurrentIndex, str);
+                CurrentIndex++;
             }
         }
         void WriteConfig()
         {
-            File.WriteAllLines(_configPath, AllSourcePaths);
+            File.WriteAllLines(_configPath, AllSourcePaths.Values);
         }
         void LoadAllApps()
         {
             ApplicationPanel.Children.Clear();
-            int CurrentIndex = 0;
-            foreach (string PathAndImage in AllSourcePaths)
+            foreach (KeyValuePair<int, string> ID_PathAndImage in AllSourcePaths)
             {
-                string[] SplittedPathAndImage = PathAndImage.Split('|');
+                string[] SplittedPathAndImage = ID_PathAndImage.Value.Split('|');
                 string App_Path = SplittedPathAndImage[0];
                 string App_ImagePath = SplittedPathAndImage[1];
                 ApplicationItemView applicationView = new ApplicationItemView(App_Path, App_ImagePath);
-                applicationView.Tag = CurrentIndex;
+                applicationView.Tag = ID_PathAndImage.Key;
+                applicationView.app_OpenApp += OpenApplicationClick;
+                applicationView.app_OpenContextMenu += OpenApplicationContextMenuClick;
+                MenuItem dltMenuItem = new MenuItem();
+                dltMenuItem.Header = "Delete";
+                dltMenuItem.Tag = ID_PathAndImage.Key;
+                dltMenuItem.Click += DeleteAppClick;
+                applicationView.ContextMenu = new ContextMenu();
+                applicationView.ContextMenu.Items.Add(dltMenuItem);
                 ApplicationPanel.Children.Add(applicationView);
-                CurrentIndex++;
             }
-            AddPlusButtonToAppPanel();
         }
+
+        private void DeleteAppClick(object sender, RoutedEventArgs e)
+        {
+            string ID = (sender as FrameworkElement).Tag.ToString();
+            AllSourcePaths.Remove(int.Parse(ID));
+            WriteConfig();
+            LoadAllApps();
+        }
+
+        private void OpenApplicationContextMenuClick(object? sender, System.EventArgs e)
+        {
+            ApplicationItemView appview = sender as ApplicationItemView;
+            if (appview == null)
+            {
+                throw new InvalidDataException(nameof(appview));
+            }
+            appview.ContextMenu.Visibility = Visibility.Visible;
+        }
+
+        private void OpenApplicationClick(object? sender, System.EventArgs e)
+        {
+            string ProcessPath = AllSourcePaths.ElementAt(
+                    int.Parse((sender as FrameworkElement).Tag.ToString())
+                        ).Value.Split('|')[0];
+
+            Process.Start(ProcessPath);
+        }
+
         /// <summary>
         /// Validates the config file, if it doesnt exist it creates it
         /// </summary>
@@ -59,15 +98,6 @@ namespace LightWeightAppLauncher
             {
                 File.Create(_configPath);
             }
-        }
-        /// <summary>
-        /// Adds plus button to app panel
-        /// </summary>
-        void AddPlusButtonToAppPanel()
-        {
-            PlusButton g = new PlusButton();
-            g.PreviewMouseDown += AddApplication;
-            ApplicationPanel.Children.Add(g);
         }
         /// <summary>
         /// button press for add application, opens dialoug for adding app
@@ -92,7 +122,7 @@ namespace LightWeightAppLauncher
             {
                 return;
             }
-            AllSourcePaths.Add(wd.UserInput_AppSource + "|" + wd.UserInput_ImageSource);
+            AllSourcePaths.Add(AllSourcePaths.ToArray().Length, wd.UserInput_AppSource + "|" + wd.UserInput_ImageSource);
             WriteConfig();
             LoadAllApps();
         }
